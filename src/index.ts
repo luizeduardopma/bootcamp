6b895c2de55eb76343d7c4f0fa8df93059524321
luizeduardopma <luizeduardopma@icloud.com>
2021-12-09 22:07:57 -0300

    Criando models + conexão com o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import http from "http";
 import express from "express";
+import mongoose from "mongoose";
+import config from "./config";
+import { Movie } from "./models/movie.model";
 
 const app = express();
 
 app.use(express.json());
-
-const port = 5000;
 
 app.get("/", (req, res) => {
   res.json({
@@ -13,6 +13,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log("Server Funcionando na porta: ", port);
+app.listen(config.PORT, async () => {
+  console.log("Server Funcionando na porta: ", config.PORT);
+  mongoose.connect(config.MONGO_URI);
 });
---
+++ src/models/user.model.ts
@@ -0,0 +1,31 @@
+import { Document, Schema, model } from "mongoose";
+
+interface UserDocument {
+  name: string;
+  email: string;
+  password: string;
+  comparePassword(preHarshPassword: string): Promise<boolean>;
+}
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model<UserDocument>("User", UserSchema);
+
+export { User };
